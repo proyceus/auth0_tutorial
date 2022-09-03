3d764da0e4a520755662371d0112c6a3f79095e0
proyceus <proyceus@gmail.com>
2022-09-03 16:28:44 -0400

    added both buttons/functionality, starting profile

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <main className="column">
+      <h1>Auth0 Login</h1>
+      <LoginButton />
+      <LogoutButton />
+    </main>
+  );
 }
 
 export default App;
